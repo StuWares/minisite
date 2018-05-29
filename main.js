1e79cd3c9881efe3f8c1b1ebc17c1b980772e2b4
Stu Wares <stuwares@outlook.com>
2018-05-29 21:20:53 +0100

    adding toggle arrows

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 $(document).ready(function(){
     $("#inner-box").hide();
     $("#weather-box").hide();
+    contactArrowToggle = "right"
+    projectsArrowToggle = "right"
 
     $("#contact-title").on("click", function(){
         $("#inner-box").toggle()
+        if (contactArrowToggle === "right"){
+        $("#contact-arrow").replaceWith("<i class='fas fa-angle-down' id='contact-arrow-down'></i>");
+        contactArrowToggle = "down"
+        }
+        else {
+            $("#contact-arrow-down").replaceWith("<i class='fas fa-angle-right' id='contact-arrow'></i>");
+            contactArrowToggle = "right"
+        }
     });
     $("#projects-title").on("click", function(){
         $("#weather-box").toggle()
+        if (projectsArrowToggle === "right"){
+            $("#projects-arrow").replaceWith("<i class='fas fa-angle-down' id='projects-arrow-down'></i>");
+            projectsArrowToggle = "down"
+        }
+        else {
+            $("#projects-arrow-down").replaceWith("<i class='fas fa-angle-right' id='projects-arrow'></i>");
+            projectsArrowToggle = "right"
+        }
+        
     });
 
 });
